24495632d9c32bf749de7bb4430991639d3852c3
IharTsykaka <35345292+IharTsykaka@users.noreply.github.com>
2018-02-19 11:03:59 +0300

    les 05 A B C1

--- src/Tsykala/lesson05/TaskC2.java
+++ src/Tsykala/lesson05/TaskC2.java
@@ -4,15 +4,25 @@ package Tsykala.lesson05;
 Задача: Написать программу, которая вводит с клавиатуры 20 чисел и выводит их в убывающем порядке.
 */
 
-public class TaskC2 {
-    //Напишите тут ваш код
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
 
+/*public class TaskC2 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        ArrayList <Integer> list = new ArrayList <Integer>();
+        int m = 0;
+        for (int i = 0; i < 20; i++) {
+            int n = sc.nextInt();
+             list.add(n);
+        }
+        Arrays.sort(list, Collections.reverseOrder());
+        for (int j = 0; j < 20; j++) {
+            System.out.println(list);
+        }
 
-
-
-
-    public static void sort(int[] array) {
-        //Напишите тут ваш код
     }
-
 }
+*/
